e9af8f60248033f1ab5c2319d902b6dd18df06d1
Giau <178288204+Gbalas855@users.noreply.github.com>
2024-09-01 12:39:53 +0700

    Add hold mouse to paint color, and add a random color button

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
+const gridContainer = document.querySelector(".grid-container")
 
 const DEFAULT_GRID_SIZE = 16;
 const DEFAULT_COLOR = "black"
 
 let userGridSize = DEFAULT_GRID_SIZE
+let userColor = DEFAULT_COLOR
 
 const createGrid = () => {
-    const gridContainer = document.querySelector(".grid-container")
     gridContainer.style.gridTemplateColumns = `repeat(${userGridSize}, 1fr)`;
 
     gridContainer.innerHTML = ""
@@ -14,21 +15,13 @@ const createGrid = () => {
         const div = document.createElement("div")
         div.classList.add("square-box")
         gridContainer.appendChild(div)
-        console.log(i)  
     }
-
-    gridContainer.addEventListener("mouseover", e => {
-        if (e.target.classList.contains("square-box")) {
-            e.target.style.backgroundColor = DEFAULT_COLOR
-        }
-      })
 }
 
 
+
 const getUserGridSize = () => {
-    const userSizeBtn = document.querySelector(".user-grid-btn")
-    userSizeBtn.addEventListener("click", () => {
-        let inputSize = parseInt(prompt("Enter grid size you want to create"))
+    let inputSize = parseInt(prompt("Enter grid size you want to create"))
 
         if (isNaN(inputSize) || inputSize < 0) {
             alert("Please enter a number")
@@ -38,9 +31,58 @@ const getUserGridSize = () => {
             userGridSize = inputSize
             createGrid()
         }
+}
+
+
+const getRandomColor = () => {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+
+const userButtons = () => {
+    const userSizeBtn = document.querySelector(".user-grid-btn")
+    const randomColorBtn = document.querySelector(".random-color")
+
+    userSizeBtn.addEventListener("click",getUserGridSize)
+
+    randomColorBtn.addEventListener("click", () => {
+        userColor = "Random"
     })
 
 }
 
-getUserGridSize()
+
+const coloring = (element) => {
+    if (userColor === "Random") {
+        element.style.backgroundColor = getRandomColor()
+    } else {
+        element.style.backgroundColor = userColor
+    }
+}
+
+let isMouseDown = false
+
+gridContainer.addEventListener("mousedown", (e) => {
+    isMouseDown = true;
+    if (e.target.classList.contains("square-box")) {
+        coloring(e.target)
+    }
+    e.preventDefault();
+})
+
+window.addEventListener("mouseup", () => isMouseDown = false)
+
+gridContainer.addEventListener("mouseover", (e) => {
+    if (isMouseDown && e.target.classList.contains("square-box")) {
+        coloring(e.target)
+    }
+})
+
+gridContainer.addEventListener("mouseup",() => isMouseDown = false )
+
+userButtons()
 createGrid()
